48862660c24f9ef1f33b446e33418fa1f2ba7e3b
achensetyawati <achenzetya@gmail.com>
2023-10-10 15:31:13 +0700

    copy laporan penjualan from bru

---
+++ Com.Everyday.Service.Pos.Lib/ViewModels/SalesDoc/SalesReportViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Everyday.Service.Pos.Lib.ViewModels.SalesDoc
+{
+    public class SalesReportViewModel
+    {
+        public string ItemCode { get; set; }
+        public string ItemName { get; set; }
+        public string ItemArticleRealizationOrder { get; set; }
+        public double Price { get; set; }
+        public double Quantity { get; set; }
+        public double Discount1 { get; set; }
+        public double Discount2 { get; set; }
+        public double Discount { get; set; }
+        public double DiscountNominal { get; set; }
+        public double SpecialDiscount { get; set; }
+        public double Total { get; set; }
+        public string IsReturn { get; set; }
+        public string Remark { get; set; }
+        public string PaymentType { get; set; }
+        public string BankName { get; set; }
+        public string Card { get; set; }
+        public double Margin { get; set; }
+        public double GrandTotal { get; set; }
+        public double SubTotal { get; set; }
+        public string Date { get; set; }
+        public string StoreCode { get; set; }
+        public string StoreName { get; set; }
+       
+        public string Code { get; set; }
+        public string _CreatedUtc { get; set; }
+    }
+}
