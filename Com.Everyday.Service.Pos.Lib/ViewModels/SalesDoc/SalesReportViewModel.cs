88f9d2d6e7e0e4dc976a042016e1876e0e5c1052
achensetyawati <achenzetya@gmail.com>
2023-11-21 15:56:45 +0700

    update sales report

--- Com.Everyday.Service.Pos.Lib/ViewModels/SalesDoc/SalesReportViewModel.cs
+++ Com.Everyday.Service.Pos.Lib/ViewModels/SalesDoc/SalesReportViewModel.cs
@@ -7,29 +7,28 @@ namespace Com.Everyday.Service.Pos.Lib.ViewModels.SalesDoc
     public class SalesReportViewModel
     {
         public string ItemCode { get; set; }
-        public string ItemName { get; set; }
+        public string Brand { get; set; }
+        public string Date { get; set; }
+        public string Category { get; set; }
+        public string Collection { get; set; }
+        public string SeasonCode { get; set; }
+        public string SeasonYear { get; set; }
         public string ItemArticleRealizationOrder { get; set; }
-        public double Price { get; set; }
+        public string ItemName { get; set; }
+        public string Color { get; set; }
+        public string Size { get; set; }
         public double Quantity { get; set; }
+        public string Location { get; set; }
+        public double OriginalCost { get; set; }
+        public double Gross { get; set; }
+        public double Nett { get; set; }
         public double Discount1 { get; set; }
         public double Discount2 { get; set; }
-        public double Discount { get; set; }
         public double DiscountNominal { get; set; }
         public double SpecialDiscount { get; set; }
-        public double Total { get; set; }
-        public string IsReturn { get; set; }
-        public string Remark { get; set; }
-        public string PaymentType { get; set; }
-        public string BankName { get; set; }
-        public string Card { get; set; }
+        public double TotalOriCost { get; set; }
+        public double TotalGross { get; set; }
+        public double TotalNett { get; set; }
         public double Margin { get; set; }
-        public double GrandTotal { get; set; }
-        public double SubTotal { get; set; }
-        public string Date { get; set; }
-        public string StoreCode { get; set; }
-        public string StoreName { get; set; }
-       
-        public string Code { get; set; }
-        public string _CreatedUtc { get; set; }
     }
 }
